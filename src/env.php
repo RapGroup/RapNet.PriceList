cdcf7a38664a12ac7a2102b15941380dfb066abc
Maksym Shvydkyi <Maksym.Shvydkyi@udelphi.com>
2023-12-06 14:18:14 +0200

    Update env.php

--- src/env.php
+++ src/env.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once(dirname(__DIR__, 1).'/vendor/autoload.php');
-
+require_once(dirname(__DIR__, 4).'/vendor/autoload.php');
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1), '.env');
 $dotenv->load();
